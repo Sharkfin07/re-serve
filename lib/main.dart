b096e34fcef9fd62f987cebbd04a3555612472c7
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-02-11 10:08:49 +0700

    feat: Applied AppTheme to the MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'presentation/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      theme: AppTheme.lightTheme(),
+      darkTheme: AppTheme.darkTheme(),
+      themeMode: ThemeMode.system,
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
